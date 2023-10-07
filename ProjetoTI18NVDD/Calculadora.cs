7cb53961724f569a4aaae66dbdf48bb85751ccbe
ELIAS ALMEIDA SANTOS <elias.asantos10@senacsp.edu.br>
2023-10-06 21:36:35 -0300

    first commit

---
+++ ProjetoTI18NVDD/Calculadora.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTI18NVDD
+{
+    internal class Calculadora
+    {
+        //variáeis globais
+        private double num1;
+        private double num2;
+
+        //Método construtor -> Instância as variavéis na memória do computador
+        public Calculadora
+        
+            num1 = 0;
+            num2 = 0;
+
+        }//Fim do método construtor
+
+        //Métodos Modificadores = GET e SET
+        public double ConsultarNum1
+        { 
+            get { return this num1; }
+            set { this num1 = value; }
+        }
+
+            public double ConsultarNum2
+            {
+                get { return this num2; }
+                set {   this num2 = value; }
+            }
+
+            //métodos
+            public double Somar()
+            {
+                return ConsultarNum1 + ConsultarNum2        
+            }//fim do somar
+
+            public double Subtrair()
+            {
+                return ConsultaNum1 - ContultarNum2
+            }
+
+            public double Multiplicar()
+             {
+                return ConsultaNum1 * ContultarNum2
+             }
+
+            public double Dividir()
+            {
+                return ConsultaNum1 / ContultarNum2
+            }
+
+}
+
---
+++ ProjetoTI18NVDD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTI18NVDD
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe seu nome");//Escreval
+            string nome = Console.ReadLine();
+            Console.WriteLine("Bem vindo " + nome);
+            Console.ReadLine();//Leia - Uso para manter o prompt de comando
+
+        }//Fim do método main
+    }//Fim da Classe Program
+}//Fim do projeto
